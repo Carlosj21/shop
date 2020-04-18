0480d4f7dd896c85584516fa2f728097669abc2a
Carlos Colmenares <carlosjcool@gmail.com>
2020-04-17 21:07:53 -0500

    Pago con wompi (fake) implementados

--- src/components/cart/CartBrief.vue
+++ src/components/cart/CartBrief.vue
@@ -1,37 +1,45 @@
 <template>
-    <div>
-      <div class="row" v-if="cartTotal > 0">
-        <div :key="i" v-for="(product, i) in cartProducts">
-          <cart-product-item :product="product"></cart-product-item>
-        </div>
+  <div>
+    <div class="row" v-if="cartTotal > 0">
+      <div :key="i" v-for="(product, i) in cartProducts">
+        <cart-product-item :product="product"></cart-product-item>
       </div>
-      <div class="row" v-else>
-        Your cart is empty :(
+    </div>
+    <div class="row" v-else>
+      Your cart is empty :(
+    </div>
+    <div class="row u-pull-right cart-total">
+      <div class="u-pull-left">
+        <wompi-checkout :cartTotal="cartTotal"></wompi-checkout>
       </div>
-      <div class="row u-pull-right cart-total">
+      <div class="u-pull-right">
         <span class="total-title">Total:</span>
         <span class="total-ammount">${{cartTotal}}</span>
       </div>
     </div>
+  </div>
 </template>
-
 <script>
 import CartProductItem from '@/components/products/CartProductItem.vue';
+import WompiCheckout from '@/components/cart/wompiCheckout.vue';
 
 export default {
   name: 'CartBrief',
   components: {
+    WompiCheckout,
     CartProductItem,
   },
   data() {
     return {
       key: 'value',
+      checkout: null,
     };
   },
   props: {
     cartProducts: {
       type: Object,
-      default: () => {},
+      default: () => {
+      },
     },
     cartTotal: {
       type: Number,
---
+++ src/components/cart/wompiCheckout.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <button class="wompi-button u-pull-left"
+              @click="processCheckout(cartTotal * 100)">
+        Paga con wompi
+      </button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'wompiCheckout',
+  props: {
+    cartTotal: {
+      type: Number,
+      default: () => 0,
+    },
+  },
+  methods: {
+    processCheckout(amount) {
+      // eslint-disable-next-line no-undef
+      this.checkout = new WidgetCheckout({
+        currency: 'COP',
+        amountInCents: amount,
+        reference: 'AD002901221',
+        publicKey: 'pub_test_Q5yDA9xoKdePzhSGeVe9HAez7HgGORGf',
+        redirectUrl: 'https://transaction-redirect.wompi.co/check', // Opcional
+      });
+      this.checkout.open((result) => {
+        const { transaction } = result;
+        console.log('Transaction ID: ', transaction.id);
+        console.log('Transaction object: ', transaction);
+      });
+    },
+  },
+};
+</script>
